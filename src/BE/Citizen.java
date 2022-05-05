568448236f5fbe968985d4b04250a3dbdd04c438
OliverSoucc <oliv572v@easv365.dk>
2022-05-05 15:07:40 +0200

    Citizen BE

--- src/BE/Citizen.java
+++ src/BE/Citizen.java
@@ -1,4 +1,26 @@
 package BE;
 
 public class Citizen {
+    private final int id;
+    private String fName;
+    private String lName;
+    private int school;
+    private int generalInfo;
+
+    public Citizen(int id, String fName, String lNAme, int school, int generalInfo) {
+        this.id = id;
+        this.fName = fName;
+        this.lName = lNAme;
+        this.school = school;
+        this.generalInfo = generalInfo;
+    }
+
+    public Citizen(int id, String fName, String lNAme, int school) {
+        this.id = id;
+        this.fName = fName;
+        this.lName = lNAme;
+        this.school = school;
+    }
+
+    
 }

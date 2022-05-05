1199b7d13efc8fdb17234d7cf02d0c823ba6fec1
Matej Kuka <mate1778@easv365.dk>
2022-05-05 11:01:52 +0200

    added components of View into LoginController

--- src/GUI/Controllers/LoginController.java
+++ src/GUI/Controllers/LoginController.java
@@ -1,7 +1,25 @@
 package GUI.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
 
+    @FXML
+    private Button buttonLogin;
+
+    @FXML
+    private Label labelBadInput;
+
+    @FXML
+    private TextField textFieldPassword;
+
+    @FXML
+    private TextField textFieldUsername;
+
     String errorMessage = "Your username or password is wrong";
+
 
 }

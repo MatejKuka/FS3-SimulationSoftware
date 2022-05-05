9d34915a6e6c0b5833e2aed19db1902ac9dbf9cc
OliverSoucc <oliv572v@easv365.dk>
2022-05-05 15:02:06 +0200

    Citizen

--- src/DAL/CitizenDAO.java
+++ src/DAL/CitizenDAO.java
@@ -1,4 +1,15 @@
 package DAL;
 
+import DAL.Connector.DBConnector;
+
+import java.io.IOException;
+
 public class CitizenDAO {
+    private DBConnector dbConnector;
+
+    public CitizenDAO() throws IOException {
+        this.dbConnector = DBConnector.getInstance();
+    }
+
+
 }

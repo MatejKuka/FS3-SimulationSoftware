24925694aff4d0f5c5735d04c919e2d58e7bde7f
pelonidas <janx803y@easv365.dk>
2022-05-06 12:17:12 +0200

    setup layout view

--- src/TestMain.java
+++ src/TestMain.java
@@ -10,17 +10,6 @@ public class TestMain extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("GUI/Views/Neviem.fxml"));
-        primaryStage.initStyle(StageStyle.UNDECORATED);
-
-        root.setOnMousePressed(event -> {
-            x = event.getSceneX();
-            y = event.getSceneY();
-        });
-
-        root.setOnMousePressed(event -> {
-            primaryStage.setX(event.getSceneX() - x);
-            primaryStage.setY(event.getSceneY() - y);
-        });
 
         primaryStage.setScene(new Scene(root, 788, 555));
         primaryStage.show();

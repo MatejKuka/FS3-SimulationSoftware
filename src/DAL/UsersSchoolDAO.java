8edaf62b5ee97a8496716ae878db1a2888abc928
OliverSoucc <oliv572v@easv365.dk>
2022-05-06 15:55:48 +0200

    Users_SchoolDAO finished

--- src/DAL/UsersSchoolDAO.java
+++ src/DAL/UsersSchoolDAO.java
@@ -1,4 +1,38 @@
 package DAL;
 
+import BE.School;
+import BE.User;
+import DAL.Connector.DBConnector;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class UsersSchoolDAO {
+
+    private DBConnector dbConnector;
+
+    public UsersSchoolDAO() throws IOException {
+        dbConnector = DBConnector.getInstance();
+    }
+
+    public void addUserToSchool(User user, School school) throws Exception {
+        String sql = "INSERT INTO Users_School VALUES (?,?)";
+        try (Connection connection = dbConnector.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, user.getUserID());
+            preparedStatement.setInt(2, school.getId());
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public void removeUserFromSchool(User user, School school) throws Exception {
+        String query = "DELETE from Users_School WHERE Users = ? AND School = ?";
+        try (Connection connection = dbConnector.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, user.getUserID());
+            preparedStatement.setInt(2, school.getId());
+            preparedStatement.execute();
+        }
+    }
 }

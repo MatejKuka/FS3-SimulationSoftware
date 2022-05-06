bfd1f95db0f30835ef2e61a5cd96283e23fefb27
OliverSoucc <oliv572v@easv365.dk>
2022-05-06 16:22:08 +0200

    GeneralInformation BE

--- src/DAL/GeneralInformationDAO.java
+++ src/DAL/GeneralInformationDAO.java
@@ -1,4 +1,13 @@
 package DAL;
 
+import DAL.Connector.DBConnector;
+
+import java.io.IOException;
+
 public class GeneralInformationDAO {
+    DBConnector dbConnector;
+
+    public GeneralInformationDAO() throws IOException {
+        dbConnector = DBConnector.getInstance();
+    }
 }

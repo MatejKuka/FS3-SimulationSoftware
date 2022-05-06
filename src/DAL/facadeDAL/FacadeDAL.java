4b006648e9433241274416623f6fcf5bc2ea2356
Matej Kuka <mate1778@easv365.dk>
2022-05-06 12:54:37 +0200

    added some methods in FacadeDAL

--- src/DAL/facadeDAL/FacadeDAL.java
+++ src/DAL/facadeDAL/FacadeDAL.java
@@ -1,4 +1,50 @@
 package DAL.facadeDAL;
 
-public class FacadeDAL {
+import BE.School;
+import BE.User;
+
+import java.util.List;
+
+public class FacadeDAL implements IFacadeDAL{
+
+
+    @Override
+    public User compareLogins(String username, String password) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUsers() throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllAdmins() throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllStudents() throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllTeacher() throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<School> getAllCustomers() throws Exception {
+        return null;
+    }
+
+    @Override
+    public School createSchool(String name, String city) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void deleteSchool(School school) throws Exception {
+
+    }
 }
--- src/DAL/facadeDAL/IFacadeDAL.java
+++ src/DAL/facadeDAL/IFacadeDAL.java
@@ -1,4 +1,44 @@
 package DAL.facadeDAL;
 
+import BE.School;
+import BE.User;
+
+import java.util.List;
+
 public interface IFacadeDAL {
+
+    //UsersSchoolDAO
+
+
+    //UserDAO
+    public User compareLogins(String username, String password) throws Exception;
+    public List<User> getAllUsers() throws Exception;
+    public List<User> getAllAdmins() throws Exception;
+    public List<User> getAllStudents() throws Exception;
+    public List<User> getAllTeacher() throws Exception;
+
+    //TeacherStudentDAO
+
+
+    //SchoolDAO
+    public List<School> getAllCustomers() throws Exception;
+    public School createSchool(String name, String city) throws Exception;
+    public void deleteSchool(School school) throws Exception;
+
+
+    //HealthConditionsDAO
+
+
+    //GeneralInformationDAO
+
+
+    //FunctionalityStateDAO
+
+
+    //CitizenAssessmentDAO
+
+
+    //CitizenDAO
+
+
 }

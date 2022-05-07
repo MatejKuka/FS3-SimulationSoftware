4ea9bb8688b12c5389603425bedc23623cb5941a
OliverSoucc <oliv572v@easv365.dk>
2022-05-07 16:02:47 +0200

    healthConditionAnsw BE

--- src/BE/HealthConditionsAnsw.java
+++ src/BE/HealthConditionsAnsw.java
@@ -1,4 +1,61 @@
 package BE;
 
 public class HealthConditionsAnsw {
+    private int id;
+    private String SaveAs;
+    private String ProfessNote;
+    private String CurrAssess;
+    private String ExpectedLvl;
+    private String FollUpDate;
+    private String ObservNote;
+    private int TypeOfCase;
+    private int Citizen;
+
+    public HealthConditionsAnsw(int id, String saveAs, String professNote, String currAssess, String expectedLvl, String follUpDate, String observNote, int typeOfCase, int citizen) {
+        this.id = id;
+        SaveAs = saveAs;
+        ProfessNote = professNote;
+        CurrAssess = currAssess;
+        ExpectedLvl = expectedLvl;
+        FollUpDate = follUpDate;
+        ObservNote = observNote;
+        TypeOfCase = typeOfCase;
+        Citizen = citizen;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSaveAs() {
+        return SaveAs;
+    }
+
+    public String getProfessNote() {
+        return ProfessNote;
+    }
+
+    public String getCurrAssess() {
+        return CurrAssess;
+    }
+
+    public String getExpectedLvl() {
+        return ExpectedLvl;
+    }
+
+    public String getFollUpDate() {
+        return FollUpDate;
+    }
+
+    public String getObservNote() {
+        return ObservNote;
+    }
+
+    public int getTypeOfCase() {
+        return TypeOfCase;
+    }
+
+    public int getCitizen() {
+        return Citizen;
+    }
 }

d8fa82d7e156680fcf83568e576f4f6d55fcd4ea
OliverSoucc <oliv572v@easv365.dk>
2022-05-07 16:03:59 +0200

    kk

--- src/DAL/HealthConditionsDAO.java
+++ src/DAL/HealthConditionsDAO.java
@@ -1,4 +1,15 @@
 package DAL;
 
+import DAL.Connector.DBConnector;
+
+import java.io.IOException;
+
 public class HealthConditionsDAO {
+
+    DBConnector dbConnector;
+
+    public HealthConditionsDAO() throws IOException {
+        dbConnector = DBConnector.getInstance();
+    }
+    
 }

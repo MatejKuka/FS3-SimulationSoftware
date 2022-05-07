3c2d4a7abff36bd44753eca696304f5858ca49ee
OliverSoucc <oliv572v@easv365.dk>
2022-05-07 21:19:20 +0200

    CitizAssDAO get

--- src/DAL/CitizensAssessmentDAO.java
+++ src/DAL/CitizensAssessmentDAO.java
@@ -1,8 +1,13 @@
 package DAL;
 
+import BE.CitizensAssessment;
+import BE.HealthConditions;
 import DAL.Connector.DBConnector;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class CitizensAssessmentDAO {
 
@@ -10,5 +15,31 @@ public class CitizensAssessmentDAO {
 
     public CitizensAssessmentDAO() throws IOException {
         dbConnector = DBConnector.getInstance();
+    }
+
+    public CitizensAssessment getCitizenAssessment(int idCitizen) throws Exception {
+        CitizensAssessment citizensAssessment = null;
+        String query =  "SELECT * FROM Citizens_Assessment WHERE Citizen = ?";
+
+        try (Connection connection = dbConnector.getConnection()){
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, idCitizen);
+            preparedStatement.execute();
+
+            ResultSet resultSet = preparedStatement.getResultSet();
+            if (resultSet.next()){
+                String performance = resultSet.getString("Performance");
+                String importance = resultSet.getString("Importance");
+                String citizWishes = resultSet.getString("CitizWishes");
+                String follUpDate = resultSet.getString("FollUpDate");
+                String observNote = resultSet.getString("ObservNote");
+                String functionalityType = resultSet.getString("FunctionalityType");
+                int citizen = resultSet.getInt("Citizen");
+
+                citizensAssessment = new CitizensAssessment(idCitizen,  performance, importance, citizWishes, follUpDate, observNote, functionalityType, citizen);
+                System.out.println(citizensAssessment);
+            }
+        }
+        return citizensAssessment;
     }
 }

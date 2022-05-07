1e8d7c403596264548a44ca4a8033d70e71a12ad
OliverSoucc <oliv572v@easv365.dk>
2022-05-07 20:21:35 +0200

    CitizenAss BE

--- src/BE/CitizensAssessment.java
+++ src/BE/CitizensAssessment.java
@@ -1,4 +1,55 @@
 package BE;
 
 public class CitizensAssessment {
+    private int id;
+    private String Performance;
+    private String Importance;
+    private String CitizWishes;
+    private String FollUpDate;
+    private String ObservNote;
+    private int FunctionalityType;
+    private int Citizen;
+
+    public CitizensAssessment(int id, String performance, String importance, String citizWishes, String follUpDate, String observNote, int functionalityType, int citizen) {
+        this.id = id;
+        Performance = performance;
+        Importance = importance;
+        CitizWishes = citizWishes;
+        FollUpDate = follUpDate;
+        ObservNote = observNote;
+        FunctionalityType = functionalityType;
+        Citizen = citizen;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPerformance() {
+        return Performance;
+    }
+
+    public String getImportance() {
+        return Importance;
+    }
+
+    public String getCitizWishes() {
+        return CitizWishes;
+    }
+
+    public String getFollUpDate() {
+        return FollUpDate;
+    }
+
+    public String getObservNote() {
+        return ObservNote;
+    }
+
+    public int getFunctionalityType() {
+        return FunctionalityType;
+    }
+
+    public int getCitizen() {
+        return Citizen;
+    }
 }

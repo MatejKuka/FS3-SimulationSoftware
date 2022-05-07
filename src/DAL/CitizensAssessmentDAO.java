1f5ae343674f40897fae04b5d3afd699f0aa8db4
OliverSoucc <oliv572v@easv365.dk>
2022-05-07 20:22:21 +0200

    kk

--- src/DAL/CitizensAssessmentDAO.java
+++ src/DAL/CitizensAssessmentDAO.java
@@ -1,4 +1,14 @@
 package DAL;
 
+import DAL.Connector.DBConnector;
+
+import java.io.IOException;
+
 public class CitizensAssessmentDAO {
+
+    DBConnector dbConnector;
+
+    public CitizensAssessmentDAO() throws IOException {
+        dbConnector = DBConnector.getInstance();
+    }
 }

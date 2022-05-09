2af569712bd61ee964bb1e7c35b8fcbc1d31be66
pelonidas <janx803y@easv365.dk>
2022-05-09 11:50:41 +0200

    Edit Profile changes

--- src/GUI/Controllers/EditProfileController.java
+++ src/GUI/Controllers/EditProfileController.java
@@ -3,6 +3,8 @@ package GUI.Controllers;
 import BE.User;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 
@@ -11,16 +13,45 @@ import java.util.ResourceBundle;
 
 public class EditProfileController implements Initializable {
     @FXML
-    private BorderPane root;
+    private BorderPane root, editBorderPane, deleteBorderPane;
     private User user;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        setupView();
-        user = new User(22, "gastanovec", "ronaldo", 2);
+        setupInitialView();
+        user = new User(22, "Jozef", "Matus", "palenka", "tranzistor", 2);
     }
 
-    private void setupView() {
-        Text text = new Text("Test");
+    private void setupInitialView() {
+        //Setup buttons we need
+        Button editButton = new Button("Edit Profile");
+        Button saveChangesButton = new Button("Save Changes");
+        Button deleteButton = new Button("Delete Profile");
+        Button cancelChangesButton = new Button("Cancel");
 
+        setupInitBorderPanes(editButton, deleteButton);
+
+        //action listeners
+        editButton.setOnAction(event -> {
+            cleanBorderPanes();
+            editBorderPane.setCenter(saveChangesButton);
+            deleteBorderPane.setCenter(cancelChangesButton);
+        });
+
+        cancelChangesButton.setOnAction(event -> {
+            cleanBorderPanes();
+
+            setupInitBorderPanes(editButton, deleteButton);
+        });
+    }
+
+    private void setupInitBorderPanes(Node editButton, Node deleteButton) {
+        editBorderPane.setCenter(editButton);
+        deleteBorderPane.setCenter(deleteButton);
+    }
+    // Clear border panes so we can add new ones
+    private void cleanBorderPanes() {
+        editBorderPane.getChildren().clear();
+        deleteBorderPane.getChildren().clear();
     }
 }

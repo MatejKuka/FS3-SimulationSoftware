c7ac181b4b97562f91cd55fcd023da9ef4c3cf18
pelonidas <janx803y@easv365.dk>
2022-05-09 12:22:45 +0200

    Edit Profile changes

--- src/GUI/Controllers/EditProfileController.java
+++ src/GUI/Controllers/EditProfileController.java
@@ -5,7 +5,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 
 import java.net.URL;
@@ -13,13 +15,16 @@ import java.util.ResourceBundle;
 
 public class EditProfileController implements Initializable {
     @FXML
-    private BorderPane root, editBorderPane, deleteBorderPane;
-    private User user;
+    private BorderPane editBorderPane, deleteBorderPane, firstNameBorderPane, lastNameBorderPane, usernameBorderPane, roleBorderPane, passwordBorderPane;
 
+    private User user;
+    private TextField firstNameTextField, lastNameTextField, usernameTextField,  passwordTextField;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        setupInitialView();
         user = new User(22, "Jozef", "Matus", "palenka", "tranzistor", 2);
+        initializeTextFields();
+        setupInitialView();
+
     }
 
     private void setupInitialView() {
@@ -30,17 +35,20 @@ public class EditProfileController implements Initializable {
         Button cancelChangesButton = new Button("Cancel");
 
         setupInitBorderPanes(editButton, deleteButton);
-
+        setupLabels();
         //action listeners
         editButton.setOnAction(event -> {
             cleanBorderPanes();
+            cleanNodes();
+            setupTextFields();
+
             editBorderPane.setCenter(saveChangesButton);
             deleteBorderPane.setCenter(cancelChangesButton);
         });
 
         cancelChangesButton.setOnAction(event -> {
             cleanBorderPanes();
-
+            setupLabels();
             setupInitBorderPanes(editButton, deleteButton);
         });
     }
@@ -53,5 +61,40 @@ public class EditProfileController implements Initializable {
     private void cleanBorderPanes() {
         editBorderPane.getChildren().clear();
         deleteBorderPane.getChildren().clear();
+    }
+    private void cleanNodes() {
+        firstNameBorderPane.getChildren().clear();
+        lastNameBorderPane.getChildren().clear();
+        usernameBorderPane.getChildren().clear();
+        passwordBorderPane.getChildren().clear();
+    }
+
+    private void setupTextFields() {
+        firstNameBorderPane.setCenter(firstNameTextField);
+        lastNameBorderPane.setCenter(lastNameTextField);
+        usernameBorderPane.setCenter(usernameTextField);
+        passwordBorderPane.setCenter(passwordTextField);
+    }
+
+    private void setupLabels() {
+        cleanNodes();
+        Text firstNameLabel = new Text(user.getFirstName());
+        Text lastNameLabel = new Text(user.getLastName());
+        Text usernameLabel = new Text(user.getLoginName());
+        Text roleLabel = new Text(String.valueOf(user.getRoleID()));
+        Text passwordLabel = new Text(user.getPassword());
+
+        firstNameBorderPane.setCenter(firstNameLabel);
+        lastNameBorderPane.setCenter(lastNameLabel);
+        usernameBorderPane.setCenter(usernameLabel);
+        roleBorderPane.setCenter(roleLabel);
+        passwordBorderPane.setCenter(passwordLabel);
+    }
+
+    private void initializeTextFields() {
+        firstNameTextField = new TextField();
+        lastNameTextField = new TextField();
+        usernameTextField = new TextField();
+        passwordTextField = new TextField();
     }
 }

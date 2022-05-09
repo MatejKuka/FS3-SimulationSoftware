99c6b533e09deefb0d4c5383dd385cc664983d52
pelonidas <janx803y@easv365.dk>
2022-05-09 10:19:49 +0200

    Edit Profile created

---
+++ src/GUI/Controllers/EditProfileController.java
@@ -0,0 +1,14 @@
+package GUI.Controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EditProfileController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}

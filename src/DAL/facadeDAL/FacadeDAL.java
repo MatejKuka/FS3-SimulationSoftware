9c90f0f4f1845b98e09c9045f5005186b63ac9ac
OliverSoucc <oliv572v@easv365.dk>
2022-05-09 14:06:42 +0200

    DALFacade

--- src/DAL/facadeDAL/FacadeDAL.java
+++ src/DAL/facadeDAL/FacadeDAL.java
@@ -1,9 +1,6 @@
 package DAL.facadeDAL;
 
-import BE.Citizen;
-import BE.GeneralInfo;
-import BE.School;
-import BE.User;
+import BE.*;
 import DAL.*;
 
 import java.io.IOException;
@@ -26,6 +23,16 @@ public class FacadeDAL implements IFacadeDAL{
     @Override
     public int returnUsersSchoolID(User user) throws Exception {
         return usersSchoolDAO.returnUsersSchoolID(user);
+    }
+
+    @Override
+    public void addUserToSchool(User user, School school) throws Exception {
+        
+    }
+
+    @Override
+    public void removeUserFromSchool(User user, School school) throws Exception {
+
     }
 
     @Override
@@ -54,6 +61,41 @@ public class FacadeDAL implements IFacadeDAL{
     }
 
     @Override
+    public User createAdmin(String firstName, String lastName, String loginName, String password) throws Exception {
+        return null;
+    }
+
+    @Override
+    public User createTeacher(String firstName, String lastName, String loginName, String password) throws Exception {
+        return null;
+    }
+
+    @Override
+    public User createStudent(String firstName, String lastName, String loginName, String password) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(User user) throws Exception {
+
+    }
+
+    @Override
+    public void updateUser(User user) throws Exception {
+
+    }
+
+    @Override
+    public void addStudentToTeacher(User student, User teacher) throws Exception {
+
+    }
+
+    @Override
+    public void removeStudentFromTeacher(User student, User teacher) throws Exception {
+
+    }
+
+    @Override
     public List<School> getAllCustomers() throws Exception {
         return null;
     }
@@ -69,13 +111,88 @@ public class FacadeDAL implements IFacadeDAL{
     }
 
     @Override
+    public void updateSchool(School school) throws Exception {
+
+    }
+
+    @Override
+    public List<HealthConditions> getHealthCondition(int idCitizen) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void updateHealthConditions(HealthConditions healthConditions) throws Exception {
+
+    }
+
+    @Override
+    public HealthConditions createHealthCondition(String SaveAs, String ProfessNote, String CurrAssess, String ExpectedLvl, String FollUpDate, String ObservNote, int TypeOfCase, int Citizen) throws Exception {
+        return null;
+    }
+
+    @Override
     public GeneralInfo getGeneralInfo(int idGeneralInfo) throws Exception {
         return generalInformationDAO.getGeneralInfo(idGeneralInfo);
     }
 
     @Override
+    public GeneralInfo createGeneralInfo(String mastery, String motivation, String resources, String roller, String habits, String educationJob, String lifeStory, String healthInfo, String assistiveDevices, String interiorOfDwelling, String network) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void updateGeneralInfo(GeneralInfo generalInfo) throws Exception {
+
+    }
+
+    @Override
+    public List<FunctionalityState> getCitizenFunctionalityState(int idCitizen) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void updateFunctionalityState(FunctionalityState functionalityState) throws Exception {
+
+    }
+
+    @Override
+    public FunctionalityState createFunctionalityState(int currLvl, int expectedLvl, String professNote, String follUpDate, int functionalityType, int citizen) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<CitizensAssessment> getCitizenAssessment(int idCitizen) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void updateCitizenAssessment(CitizensAssessment citizensAssessment) throws Exception {
+
+    }
+
+    @Override
+    public CitizensAssessment createCitizensAssessment(String performance, String importance, String citizWishes, String follUpDate, String observNote, int functionalityType, int citizen) throws Exception {
+        return null;
+    }
+
+    @Override
     public List<Citizen> getAllCitizenFromOneSchool(int schoolId) throws Exception {
         return citizenDAO.getAllCitizenFromOneSchool(schoolId);
+    }
+
+    @Override
+    public void updateCitizen(Citizen citizen) throws Exception {
+
+    }
+
+    @Override
+    public void deleteCitizen(Citizen citizen, int generalInfoIdOfCitizen) throws Exception {
+
+    }
+
+    @Override
+    public Citizen createCitizen(String fName, String lName, int school, int generalInfo) throws Exception {
+        return null;
     }
 
 

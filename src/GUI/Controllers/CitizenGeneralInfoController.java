7bac0a71c2ab326e204db9a034209780d9863a0b
Matej Kuka <mate1778@easv365.dk>
2022-05-13 00:43:36 +0200

    labeled components from CitizenGeneralInfoView

--- src/GUI/Controllers/CitizenGeneralInfoController.java
+++ src/GUI/Controllers/CitizenGeneralInfoController.java
@@ -1,4 +1,78 @@
 package GUI.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 public class CitizenGeneralInfoController {
+
+    @FXML
+    private Button btnAssistDevSec;
+
+    @FXML
+    private Button btnDwelSec, btnEduJobSec, btnHabitsSec, btnHealtInfoSec, btnLifeStorySec, btnMasterySec, btnMotivationSec, btnNetworkSec, btnResourcesSec, btnRollerSec;
+
+    @FXML
+    private Label labelExplanation, labelName;
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    void toAssistDevSec(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toDwelSec(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toEduJobSec(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHabitSec(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHealthInfoSec(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toLifeStorySec(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toMasterySec(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toMotivationSec(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toNetworkSec(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toResourceSec(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toRollerSec(ActionEvent event) {
+
+    }
+
 }

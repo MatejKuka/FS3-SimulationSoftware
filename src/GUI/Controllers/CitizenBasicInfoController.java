3866df4a11a6698f2ee242953324f9130ef7ab3c
pelonidas <janx803y@easv365.dk>
2022-05-17 09:49:27 +0200

    Summary (required)

--- src/GUI/Controllers/CitizenBasicInfoController.java
+++ src/GUI/Controllers/CitizenBasicInfoController.java
@@ -5,26 +5,20 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CitizenBasicInfoController implements Initializable {
-
     @FXML
-    private Button btnBack;
-
+    private Label labelAge, labelFirstName, labelLastName;
     @FXML
-    private Button btnEdit;
-
+    private BorderPane firstNameContainer, lastNameContainer, ageContainer;
     @FXML
-    private Label labelAge;
-
-    @FXML
-    private Label labelFirstName;
-
-    @FXML
-    private Label labelLastName;
+    private HBox buttonsContainer;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -36,11 +30,31 @@ public class CitizenBasicInfoController implements Initializable {
 
     }
 
-    @FXML
-    void toEdit(ActionEvent event) {
-
+    public void handleEditButton(ActionEvent event) {
+        clearLabels();
+        clearButtons();
+        setupTextFields();
     }
 
+    private void clearLabels() {
+        firstNameContainer.getChildren().clear();
+        lastNameContainer.getChildren().clear();
+        ageContainer.getChildren().clear();
+    }
+    private void clearButtons() {
+        buttonsContainer.getChildren().clear();
+    }
+    private void setupTextFields() {
+        TextField firstNameTextField = new TextField();
+        TextField lastNameTextField = new TextField();
+        TextField ageTextField = new TextField();
 
-
+        firstNameContainer.setCenter(firstNameTextField);
+        lastNameContainer.setCenter(lastNameTextField);
+        ageContainer.setCenter(ageTextField);
+    }
+   private void setupButtons() {
+        Button saveButton = new Button("Save");
+        Button cancelButton = new Button("Cancel");
+   }
 }

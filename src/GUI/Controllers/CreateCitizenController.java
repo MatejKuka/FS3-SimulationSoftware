e78adb23757a5d5f951c4fe04134351a4ca5863c
pelonidas <janx803y@easv365.dk>
2022-05-24 09:23:09 +0200

    ss

--- src/GUI/Controllers/CreateCitizenController.java
+++ src/GUI/Controllers/CreateCitizenController.java
@@ -5,7 +5,7 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class CreateBasicInfoViewController implements Initializable {
+public class CreateCitizenController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {

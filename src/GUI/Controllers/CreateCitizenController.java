18f0fbbd43481b3fdf109f21ffc762967cfe8a48
pelonidas <janx803y@easv365.dk>
2022-05-24 09:37:31 +0200

    Update CreateCitizenController.java

--- src/GUI/Controllers/CreateCitizenController.java
+++ src/GUI/Controllers/CreateCitizenController.java
@@ -1,14 +1,25 @@
 package GUI.Controllers;
 
+import BE.School;
+import GUI.Models.MainModel;
 import javafx.fxml.Initializable;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class CreateCitizenController implements Initializable {
-
+    private MainModel mainModel;
+    private List<School> schoolList;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        try {
+            mainModel = new MainModel();
+            schoolList = mainModel.getAllSchools();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }

23f9216b0302b478a6d46e3d60fc26ae6ea37618
Matej Kuka <mate1778@easv365.dk>
2022-05-25 14:25:17 +0200

    tableVIews for students

--- src/GUI/Controllers/StudentCaseController.java
+++ src/GUI/Controllers/StudentCaseController.java
@@ -1,5 +1,7 @@
 package GUI.Controllers;
 
+import BE.Citizen;
+import GUI.Models.MainModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -7,12 +9,16 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class StudentCaseController implements Initializable {
+    Citizen clickedCitizen;
+    MainModel mainModel;
 
     @FXML
     private Button btnFillCase;
@@ -24,27 +30,42 @@ public class StudentCaseController implements Initializable {
     private Label labelLifeStory;
 
     @FXML
-    private TableColumn<?, ?> tableColFName;
+    private TableColumn<Citizen, String> tableColFName;
 
     @FXML
-    private TableColumn<?, ?> tableColId;
+    private TableColumn<Citizen, Integer> tableColId;
 
     @FXML
-    private TableColumn<?, ?> tableColLName;
+    private TableColumn<Citizen, String> tableColLName;
 
     @FXML
-    private TableView<?> tableViewCases;
+    private TableView<Citizen> tableViewCases;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        try {
+            mainModel = new MainModel();
+            setUpTableView();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 
+    void setUpTableView() throws Exception {
+        tableColId.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        tableColFName.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        tableColLName.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        //tableViewCases.getItems().setAll();
+    }
 
 
     @FXML
     void selectedCase(MouseEvent event) {
-
+        clickedCitizen = tableViewCases.getSelectionModel().getSelectedItem();
     }
 
     @FXML

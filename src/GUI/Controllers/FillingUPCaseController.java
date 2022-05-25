8e2a7fd83b086cc8bee5ccb803ee2cfb6271846c
Matej Kuka <mate1778@easv365.dk>
2022-05-25 20:17:52 +0200

    created View for filling up cases + controller

---
+++ src/GUI/Controllers/FillingUPCaseController.java
@@ -0,0 +1,278 @@
+package GUI.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
+public class FillingUPCaseController {
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private Button btnEdit;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private ComboBox<?> comboBoxExpLevel;
+
+    @FXML
+    private ComboBox<?> comboBoxSaving;
+
+    @FXML
+    private DatePicker datePickerFollDate;
+
+    @FXML
+    private Label labelCurrAss;
+
+    @FXML
+    private Label labelFName;
+
+    @FXML
+    private Label labelLName;
+
+    @FXML
+    private Label labelNameHC;
+
+    @FXML
+    private Label labelObNotes;
+
+    @FXML
+    private Label labelProfNote;
+
+    @FXML
+    void toCancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toEdit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC10(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC11(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC12(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC13(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC14(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC15(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC16(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC17(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC18(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC19(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC20(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC21(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC22(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC23(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC24(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC25(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC26(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC27(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC28(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC29(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC30(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC31(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC32(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC33(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC34(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC35(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC36(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC37(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC38(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC39(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC4(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC40(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC41(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC42(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC43(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC5(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC6(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC7(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC8(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHC9(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toSave(ActionEvent event) {
+
+    }
+
+}

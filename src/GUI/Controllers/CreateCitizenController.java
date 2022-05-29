a02d7506781d8b3371b0c819a900f41a43ea4ec4
pelonidas <janx803y@easv365.dk>
2022-05-29 19:47:05 +0200

    Update CreateCitizenController.java

--- src/GUI/Controllers/CreateCitizenController.java
+++ src/GUI/Controllers/CreateCitizenController.java
@@ -1,23 +1,15 @@
 package GUI.Controllers;
 
-;
 import BE.Citizen;
-import BE.School;
 import BLL.exeptions.UserException;
 import GUI.Models.MainModel;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
 import java.net.URL;
-import java.util.List;
-import java.util.Objects;
 import java.util.ResourceBundle;
-import java.util.stream.Collectors;
 
 public class CreateCitizenController implements Initializable {
     @FXML
@@ -26,19 +18,17 @@ public class CreateCitizenController implements Initializable {
     private Button cancelButton, saveButton;
 
     private MainModel mainModel;
-    private List<School> schoolList;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
             mainModel = new MainModel();
-            schoolList = mainModel.getAllSchools();
-        } catch (Exception | UserException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void handleSaveButton(ActionEvent event) throws UserException {
+    public void handleSaveButton() throws UserException {
         if (firstNameTextField.getText().equals("") || lastNameTextField.getText().equals("")) {
             if (firstNameTextField.getText().equals("")) firstNameTextField.getStyleClass().add("custom-text-field-error");
             if (lastNameTextField.getText().equals("")) lastNameTextField.getStyleClass().add("custom-text-field-error");
@@ -53,7 +43,7 @@ public class CreateCitizenController implements Initializable {
         }
     }
 
-    public void handleCancelButton(ActionEvent event) {
+    public void handleCancelButton() {
         Stage stage = (Stage) cancelButton.getScene().getWindow();
         stage.close();
     }
